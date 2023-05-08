98c51538c98810ca72f201956809951050b4de81
mihir <mp06121@gmail.com>
2023-05-08 09:08:06 +0530

    initial commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pract_1.Models;
+
+namespace pract_1.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student student = new Student()
+            {
+                Name = "Mihir",
+                Id = 1,
+                Gender = "Male"
+            };
+
+            return View(student);
+        }
+
+    }
+}
